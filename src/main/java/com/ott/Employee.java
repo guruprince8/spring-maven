abc03db8777af5f2072b37ecff9ae2d796375a55
guruprince8 <guru.prince8@gmail.com>
2023-03-01 09:36:49 +0530

    application context changes

---
+++ src/main/java/com/ott/Employee.java
@@ -0,0 +1,39 @@
+package com.ott;
+
+public class Employee {
+
+    private int id;
+    private String name;
+    private Address address;
+
+    public Employee() {
+
+    }
+
+    public Employee(int id, String name, Address address){
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "[id="+this.id+",name="+this.name+"]";
+    }
+}
--- src/main/java/com/ott/OttPlatformServicesMavenApplication.java
+++ src/main/java/com/ott/OttPlatformServicesMavenApplication.java
@@ -1,12 +1,22 @@
 package com.ott;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @SpringBootApplication
 public class OttPlatformServicesMavenApplication {
 
 	public static void main(String[] args) {
+		// using application context xml
+		Resource r=new ClassPathResource("applicationContext.xml");
+		BeanFactory factory=new XmlBeanFactory(r);
+		Employee employee = (Employee) factory.getBean("employee");
+		System.out.println(employee.toString());
+		// initialize spring boot container
 		SpringApplication.run(OttPlatformServicesMavenApplication.class, args);
 	}
 
